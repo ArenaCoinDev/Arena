e9d8e906967428e2a4daf5664e93e712e4f6d83d
root <eric.le.roy@free.Fr>
2018-09-20 16:17:35 +0200

    fix files permission issue

---
+++ fix_permissions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+chmod +x arena-master/autogen.sh
+chmod +x arena-master/share/genbuild.sh
+chmod +x arena-master/src/secp256k1/autogen.sh
+chmod +x arena-master/depends/config.guess
+chmod +x arena-master/depends/config.sub
+chmod +x arena-master/contrib/macdeploy/macdeployqtplus
+
+
+
