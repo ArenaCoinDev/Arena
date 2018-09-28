44f308dd223210598fdcceaed93abe70cf979f9b
Eric <eric.le.roy@free.fr>
2018-09-28 09:45:46 +0200

    splashcreen udpate

--- arena-master/src/qt/splashscreen.cpp
+++ arena-master/src/qt/splashscreen.cpp
@@ -37,7 +37,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
 
     // set reference point, paddings
     int paddingLeft             = 14;
-    int paddingTop              = 470;
+    int paddingTop              = 480;
     int titleVersionVSpace      = 17;
     int titleCopyrightVSpace    = 32;
 
